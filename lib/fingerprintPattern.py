fe645a7b999dde934a06046ad596a8fd76947196
Arthur Tofani <gramofone@gmail.com>
2017-11-28 12:43:28 -0200

    pres

---
+++ lib/fingerprintPattern.py
@@ -0,0 +1,11 @@
+class FingerprintPattern:
+  def __init__(audioSineModel):
+    self.audioSineModel = audioSineModel
+    self.elements_found = []
+
+  def collect():
+    # start collecting all ocurrences of a specified pattern
+
+  def plot(plt):
+    # adds gra
+    return
---
+++ lib/patterns/whiteBlock.py
@@ -0,0 +1,3 @@
+class WhiteBlock < FingerprintPattern:
+  def collect():
+    # finds blank blocks into the audio sine model matrix
